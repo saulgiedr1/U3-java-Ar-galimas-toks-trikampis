289afdbff3fbe2638eefe5a93b11310d8481bdba
saulgiedr1 <saulgiedr@gmail.com>
2022-06-24 11:53:19 +0300

    baigta

--- src/Programa1.java
+++ src/Programa1.java
@@ -1,29 +1,36 @@
 public class Programa1 {
     public static void main(String[] args) {
 
-        double pirma = 6;
+        double pirma = 15;
         double antra = 6;
-        double trecia = 12;
+        double trecia = 6;
 
 
         boolean arTinka = arTinkaIlgiai(pirma, antra, trecia);
-        boolean arTinka3 = arTinkaIlgiai(antra, pirma, trecia);
-        boolean arTinka4 = arTinkaIlgiai(trecia, pirma, antra);
+        boolean arTinka1 = arTinkaIlgiai(antra, pirma, trecia);
+        boolean arTinka2 = arTinkaIlgiai(trecia, pirma, antra);
+
+        boolean arGalimas= arGalimasTrikampis(arTinka, arTinka1, arTinka2);
 
 
 
-        System.out.println("Ar galimas toks trikampis = " + arTinka);
-        System.out.println("Ar galimas toks trikampis = " + arTinka3);
-        System.out.println("Ar galimas toks trikampis = " + arTinka4);
+
+
+        System.out.println("Ar tenkinama salyga = " + arTinka);
+        System.out.println("Ar tenkinama salyga = " + arTinka1);
+        System.out.println("Ar tenkinama salyga = " + arTinka2);
+
+        System.out.println("Ar galimas toks trikampis = " + arGalimas);
 
     }
 
     public static boolean arTinkaIlgiai(double a, double b, double c) {
         return (a < b + c);
     }
+    public static boolean arGalimasTrikampis(boolean a, boolean b, boolean c){
+        
+        return a == b == c;
 
-
-
-
+    }     
 
 }
