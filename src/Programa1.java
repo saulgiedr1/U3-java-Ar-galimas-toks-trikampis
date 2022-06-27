99173b9e917c1d47fd8e04f909ca5e28ac166ec7
saulgiedr1 <saulgiedr@gmail.com>
2022-06-27 17:45:26 +0300

    geresnis ir trumpesnis variantas

--- src/Programa1.java
+++ src/Programa1.java
@@ -2,35 +2,36 @@ public class Programa1 {
     public static void main(String[] args) {
 
         double pirma = 15;
-        double antra = 6;
-        double trecia = 6;
+        double antra = 150;
+        double trecia = 16;
 
 
         boolean arTinka = arTinkaIlgiai(pirma, antra, trecia);
-        boolean arTinka1 = arTinkaIlgiai(antra, pirma, trecia);
-        boolean arTinka2 = arTinkaIlgiai(trecia, pirma, antra);
+      //  boolean arTinka1 = arTinkaIlgiai(antra, pirma, trecia);
+      //  boolean arTinka2 = arTinkaIlgiai(trecia, pirma, antra);
 
-        boolean arGalimas= arGalimasTrikampis(arTinka, arTinka1, arTinka2);
+      //  boolean arGalimas= arGalimasTrikampis(arTinka, arTinka1, arTinka2);
 
 
 
 
 
         System.out.println("Ar tenkinama salyga = " + arTinka);
-        System.out.println("Ar tenkinama salyga = " + arTinka1);
-        System.out.println("Ar tenkinama salyga = " + arTinka2);
+      //  System.out.println("Ar tenkinama salyga = " + arTinka1);
+      //  System.out.println("Ar tenkinama salyga = " + arTinka2);
 
-        System.out.println("Ar galimas toks trikampis = " + arGalimas);
+       // System.out.println("Ar galimas toks trikampis = " + arGalimas);
 
     }
 
     public static boolean arTinkaIlgiai(double a, double b, double c) {
-        return (a < b + c);
+
+        return (a < b + c) && (b < a + c) && (c< a + b );
     }
-    public static boolean arGalimasTrikampis(boolean a, boolean b, boolean c){
+   // public static boolean arGalimasTrikampis(boolean a, boolean b, boolean c){
         
-        return a == b == c;
+      //  return a == b == c;
 
     }     
 
-}
+//}
